b8ab7ef5cd3ad5b254832534bfb0d762918d9a92
Wilderysb <wilderysb@hotmail.com>
2022-02-11 01:44:40 +0000

    10 Primer Commit
    ok

---
+++ Consola/ChangeString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Consola
+{
+    internal class ChangeString
+    {
+        public string build(string escrito) {
+
+            var arrayAlfabeto = new string[] {"a","b","c","d","e","f","g",
+                "h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u"
+                ,"v","w","x","y","z"};
+
+            var contador = 0;
+
+            int indice;
+
+            var escritoSplit = escrito.ToLower().ToCharArray();
+
+            foreach (char c in escritoSplit)
+            {
+
+                indice = Array.IndexOf(arrayAlfabeto, c.ToString());
+
+                if (indice >= 0)
+                {
+                    escritoSplit[contador] = char.Parse(arrayAlfabeto[indice + 1]);
+                }
+
+
+                contador++;
+            }
+            string.Join("", escritoSplit);
+        }
+    }
+
+    
+}
---
+++ Consola/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+            Console.WriteLine();
+
+            Console.Read();
+
+        }
+
+
+
+    
+    }
+}
